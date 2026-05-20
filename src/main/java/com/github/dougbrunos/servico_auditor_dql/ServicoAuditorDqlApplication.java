0c09ce2141c392173f11a23c4c38a4938b071f60
Douglas Bruno <dougbrunos@gmail.com>
2026-05-20 12:02:54 -0300

    init

---
+++ src/main/java/com/github/dougbrunos/servico_auditor_dql/ServicoAuditorDqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.dougbrunos.servico_auditor_dql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoAuditorDqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoAuditorDqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dougbrunos/servico_auditor_dql/ServicoAuditorDqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dougbrunos.servico_auditor_dql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoAuditorDqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
